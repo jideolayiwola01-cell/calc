8dd36026a90a4599d60b8805df9e6143c65a316c
jideolayiwola <jideolayiwola@Mac.lesaclub.com>
2026-04-13 21:30:34 +0100

    Add Python CI workflow

--- calk.py
+++ calk.py
@@ -1,8 +1,8 @@
-import tkinter 
+import tkinter
 
 button_values = [
-    ["AC", "+/-", "%", "÷"], 
-    ["7", "8", "9", "×"], 
+    ["AC", "+/-", "%", "÷"],
+    ["7", "8", "9", "×"],
     ["4", "5", "6", "-"],
     ["1", "2", "3", "+"],
     ["0", ".", "√", "="]
@@ -11,48 +11,18 @@ button_values = [
 right_symbols = ["÷", "×", "-", "+", "="]
 top_symbols = ["AC", "+/-", "%"]
 
-row_count = len(button_values) #5
-column_count = len(button_values[0]) #4
-
 color_light_gray = "#D4D4D2"
 color_black = "#1C1C1C"
 color_dark_gray = "#505050"
 color_orange = "#FF9500"
 color_white = "white"
 
-#window setup
-window = tkinter.Tk() #create the window
-window.title("Calculator")
-window.resizable(False, False)
 
-frame = tkinter.Frame(window)
-label = tkinter.Label(frame, text="0", font=("Arial", 45), background=color_black,
-                      foreground=color_white, anchor="e", width=column_count)
+def remove_zero_decimal(num):
+    if num % 1 == 0:
+        num = int(num)
+    return str(num)
 
-label.grid(row=0, column=0, columnspan=column_count, sticky="we")
-
-for row in range(row_count):
-    for column in range(column_count):
-        value = button_values[row][column]
-        button = tkinter.Button(frame, text=value, font=("Arial", 30),
-                                width=column_count-1, height=1,
-                                command=lambda value=value: button_clicked(value))
-        
-        if value in top_symbols:
-            button.config(foreground=color_black, background=color_light_gray)
-        elif value in right_symbols:
-            button.config(foreground=color_white, background=color_orange)
-        else:
-            button.config(foreground=color_white, background=color_dark_gray)
-        
-        button.grid(row=row+1, column=column)
-
-frame.pack()
-
-#A+B, A-B, A*B, A/B
-A = "0"
-operator = None
-B = None
 
 def clear_all():
     global A, B, operator
@@ -60,10 +30,6 @@ def clear_all():
     operator = None
     B = None
 
-def remove_zero_decimal(num):
-    if num % 1 == 0:
-        num = int(num)
-    return str(num)
 
 def button_clicked(value):
     global right_symbols, top_symbols, label, A, B, operator
@@ -83,54 +49,85 @@ def button_clicked(value):
                     label["text"] = remove_zero_decimal(numA * numB)
                 elif operator == "÷":
                     label["text"] = remove_zero_decimal(numA / numB)
-                
+
                 clear_all()
 
-        elif value in "+-×÷": #500 +, *
+        elif value in "+-×÷":
             if operator is None:
                 A = label["text"]
                 label["text"] = "0"
                 B = "0"
-            
             operator = value
 
     elif value in top_symbols:
         if value == "AC":
             clear_all()
             label["text"] = "0"
-
         elif value == "+/-":
             result = float(label["text"]) * -1
             label["text"] = remove_zero_decimal(result)
-
         elif value == "%":
             result = float(label["text"]) / 100
-            label["text"] = remove_zero_decimal(result)           
-        
-    else: #digits or .
+            label["text"] = remove_zero_decimal(result)
+
+    else:
         if value == ".":
             if value not in label["text"]:
                 label["text"] += value
-
         elif value in "0123456789":
             if label["text"] == "0":
-                label["text"] = value #replace 0
+                label["text"] = value
             else:
-                label["text"] += value #append digit
+                label["text"] += value
 
 
+if __name__ == "__main__":
+    row_count = len(button_values)
+    column_count = len(button_values[0])
 
-#center the window
-window.update() #update window with the new size dimensions
-window_width = window.winfo_width()
-window_height = window.winfo_height()
-screen_width = window.winfo_screenwidth()
-screen_height = window.winfo_screenheight()
+    # Window setup
+    window = tkinter.Tk()
+    window.title("Calculator")
+    window.resizable(False, False)
 
-window_x = int((screen_width/2) - (window_width/2))
-window_y = int((screen_height/2) - (window_height/2))
+    frame = tkinter.Frame(window)
+    label = tkinter.Label(frame, text="0", font=("Arial", 45), background=color_black,
+                          foreground=color_white, anchor="e", width=column_count)
+    label.grid(row=0, column=0, columnspan=column_count, sticky="we")
 
-#format "(w)x(h)+(x)+(y)"
-window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+    for row in range(row_count):
+        for column in range(column_count):
+            value = button_values[row][column]
+            button = tkinter.Button(frame, text=value, font=("Arial", 30),
+                                    width=column_count-1, height=1,
+                                    command=lambda value=value: button_clicked(value))
 
-window.mainloop()
+            if value in top_symbols:
+                button.config(foreground=color_black,
+                              background=color_light_gray)
+            elif value in right_symbols:
+                button.config(foreground=color_white, background=color_orange)
+            else:
+                button.config(foreground=color_white,
+                              background=color_dark_gray)
+
+            button.grid(row=row+1, column=column)
+
+    frame.pack()
+
+    A = "0"
+    operator = None
+    B = None
+
+    # Center the window
+    window.update()
+    window_width = window.winfo_width()
+    window_height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    window_x = int((screen_width/2) - (window_width/2))
+    window_y = int((screen_height/2) - (window_height/2))
+    window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+    window.mainloop()
